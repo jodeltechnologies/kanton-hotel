1fdda6d8d3050ecfe0cf2e8076ea9e82230a2a71
Jodel Technologies <your@email.com>
2026-09-07 17:34:31 +0100

    Kanton Hotel reservation system

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
     {
       cookies: {
         getAll: () => request.cookies.getAll(),
-        setAll: (list) => {
+        setAll: (list: { name: string; value: string; options?: any }[]) => {
           list.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request });
           list.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
